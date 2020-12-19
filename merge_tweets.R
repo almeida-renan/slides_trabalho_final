b03d3f2813acab564beaadce1af00924b05d0c85
Renan Almeida <renan.sralmeida@gmail.com>
2020-12-18 21:01:09 -0300

    página com slides

---
+++ merge_tweets.R
@@ -0,0 +1,46 @@
+# chamando os pacotes
+require(tidyverse)
+library(readr)
+
+`%notin%` <- Negate(`%in%`)
+
+# lendo as bases de dados
+Bolsonaro <- read_csv("jairbolsonaro.csv")
+Marina <- read_csv("MarinaSilva.csv")
+Alckmin <- read_csv("geraldoalckmin.csv")
+Haddad <- read_csv("Haddad_Fernando.csv")
+Lula <- read_csv("LulaOficial.csv")
+Boulos <- read_csv("GuilhermeBoulos.csv")
+Ciro <- read_csv("cirogomes.csv")
+
+# unindo as bases em um único data frame
+combinado <- bind_rows(
+  Bolsonaro %>%
+    mutate(person = "Bolsonaro"),
+  Marina %>%
+    mutate(person = "Marina"),
+  Alckmin %>%
+    mutate(person = "Alckmin"),
+  Boulos %>%
+    mutate(person = "Boulos"),
+  Haddad %>%
+    mutate(person = "Haddad"),
+  Lula %>%
+    mutate(person = "Lula"),
+  Ciro %>%
+    mutate(person = "Ciro"),
+) # criando a variável "person"
+
+
+combinado <- combinado %>% 
+  mutate(periodo_eleitoral = case_when(
+    (combinado$date < as.Date('2018-10-29'))&(combinado$date > as.Date('2018-07-07')) ~ "Sim",
+                                                                         T ~ "Não"))
+
+ggplot(data = combinado, aes(x = date)) +
+  geom_histogram(aes(fill = ..count..)) +
+  theme(legend.position = "none") +
+  xlab("Tempo") + ylab("Número de tweets") + 
+  scale_fill_gradient(low = "darkblue", high = "darkorange")
+
+write.csv(combinado, 'tweets_presidenciaveis.csv', row.names = F)
